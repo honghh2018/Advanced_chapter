17c037e41c4eadce48a2822350f1492e596b5cf1
Alsace <41668708+honghh2018@users.noreply.github.com>
2019-04-08 17:17:19 +0800

    Create Hard_reference_and_symbilic_reference(soft reference).pl

---
+++ Hard_reference_and_symbilic_reference(soft reference).pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+#use strict; strict declaration should be inhibited within symbolic reference 
+no strict 'refs'; ##no strict syntax claim symbolic reference worked,so strict synatx had symoblic reference syntax invalid.
+
+$name="some";
+$$name="helloworld"; ##symbolic reference
+print $some."\n";
+
+##dynamic build variables through symbolic reference, which its example show below
+for (1..5){
+   $name="$_";
+   ${"var".$name}="Hello,this is VAR $_!";
+ }
+print $var3;  #Hello,this is VAR 3
+
+#hard reference was built with symbol '\' like below
+my $a="helloworld";
+my $b=\$a;
+
