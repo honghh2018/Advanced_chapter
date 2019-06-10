1963fe0d02dd566eed1a91f4b97f9d7eb6a0ba35
Alsace <41668708+honghh2018@users.noreply.github.com>
2019-06-10 11:25:48 +0800

    Create genebank2gff3.pl

---
+++ genebank2gff3.pl
@@ -0,0 +1,3 @@
+The scripts named above comprised bp_genebank2gff3.pl for transferring genebank file from genome into gff3. 
+
+https://metacpan.org/pod/release/CJFIELDS/BioPerl-1.007001/scripts/Bio-DB-GFF/bp_genbank2gff3.pl
